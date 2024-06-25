1a3a543b01728d40f564927a32c0596d06cd090c
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-25 15:58:11 +0200

    Feat: add thumbnails with Vue

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ createApp( {
     data() {   
         return {
             slides: [
-                {
+                    {
                         image: 'img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
